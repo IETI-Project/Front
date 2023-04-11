4ad6a59050e5d20c55fbfbe5a8f50aa714f52033
lauraval19 <jennyyana@hotmail.com>
2023-04-10 21:10:10 -0500

    fetch

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {FetchUserRegister} from './components/FetchUserRegister'
+
+function App() {
+  return (<div>
+      <FetchUserRegister></FetchUserRegister>
+     "hola" </div>)
+}
+
+export default App
