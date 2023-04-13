affc61a88e1d4b056e31bd776eb6a9d8561c8bd1
CarlosOrduz777 <carlos.orduz@mail.escuelaing.edu.co>
2023-04-13 13:04:23 -0500

    merge conflicts resolved

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App/>
-      </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
